e1ecd66c2a149d63badbc4c64ef3a3482441a10d
Jonathan Ouellet <ouelletjon@gmail.com>
2013-01-31 18:44:59 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hacklendar::Application.config.secret_token = 'b4f8c6d16f61538d6f1b3e98df5de21415f0bbe603242427d623022edeb41a194e20424598f1569c8d2f424d7fe8e5d201b2222bbf6d2e1f03e157a6f491865e'
